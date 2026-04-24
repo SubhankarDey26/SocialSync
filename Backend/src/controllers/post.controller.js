e2de69a8fb4afda19c60cfcb3c1bda717fc2bf6e
subhankar <bnk.subhankar@gmail.com>
2026-04-24 10:05:45 +0530

    Implemented User Features

--- Backend/src/controllers/post.controller.js
+++ Backend/src/controllers/post.controller.js
@@ -85,6 +85,51 @@ async function getPostController(req, res) {
 
 }
 
+
+async function getPostDetails(req,res){
+    const token=req.cookies.token
+
+    if(!token)
+    {
+        return res.status(401).json({
+            message:"Unautorized Access"
+        })
+    }
+
+    let decoded
+
+    try{
+        decoded=jwt.verify(token,process.env.JWT_SECRET)
+    }catch(err){
+        return res.status(401).json({
+            message:"Invalid Token"
+        })
+    }
+
+    const userId=decoded.id
+    const postId=req.params.postId
+
+    const post=await postModel.findById(postId)
+    if(!post){
+        return res.status(404).json({
+            message:"post not Found"
+        })
+    }
+
+    const isValidUser=post.user.toString()===userId
+    if(!isValidUser){
+        return res.status(403).json({
+            message:"Forbidden Content"
+        })
+    }
+
+    return res.status(200).json({
+        message:"Post Fetched Sucessfully",
+        post
+    })
+}
+
+
 module.exports = {
-    createPostController
+    createPostController,getPostController,getPostDetails
 }
--- Backend/src/routes/post.routes.js
+++ Backend/src/routes/post.routes.js
@@ -8,4 +8,9 @@ const upload=multer({storage:multer.memoryStorage()})
 
 postRouter.post("/",upload.single("image"),postController.createPostController)
 
+
+postRouter.get("/",postController.getPostController)
+
+postRouter.get("/details/:postId",postController.getPostDetails)
+
 module.exports=postRouter
