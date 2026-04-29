a0130da1b15c8631c0090f05d75dc2179067b674
subhankar <bnk.subhankar@gmail.com>
2026-04-29 19:57:33 +0530

    Register UI Completed

--- Frontend/src/features/Auth/pages/Register.jsx
+++ Frontend/src/features/Auth/pages/Register.jsx
@@ -1,18 +1,29 @@
 import "../styles/form.scss";
 import {Link} from 'react-router'
-import axios from 'axios'
 import { useState } from "react";
+import {useAuth} from "../hooks/useAuth"
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
 
+  const {loading,HandleRegister}=useAuth()
   const [username, setusername] = useState("")
   const [email, setemail] = useState("")
   const [password, setpassword] = useState("")
+  const navigate=useNavigate()
 
   async function handleSubmit(e){
     e.preventDefault()
+    await HandleRegister(username,email,password)
+    navigate('/')
   }
 
+    if(loading)
+  {
+    return (<main>
+      <h1>Loading....</h1>
+    </main>)
+  }
 
   return (
     <main className="login-page">
